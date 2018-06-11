b7200c7f2207774e71521803c215b546fd64a72f
ItLordLeon <leonphilipp.berberich@web.de>
2018-06-11 16:34:03 +0200

    Variablen dabei
    kleiner wert pixel ... code ausgeklammert

---
+++ Variablen.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 07 17:22:56 2018
+
+@author: Leon
+"""
+
+class Variables:
+    
+    #0...255 Helligkeitsunterschied zwischen Rauschen und Bild 
+    kontrastDesZeichens = 5
+    
+    #Bild muss im Ordner des Projekts liegen
+    bildname = "Ababa.jpg"
+    
+    ##Rauschmatrix
+    #Mittelwert
+    mittelwert = 170
+    #Standartabweichung
+    standartabweichung = 20
+    
+    #Zeit ab welcher der versuch als ungültig markiert
+    
+    #Zeit des Stimulus
+    
+    #Zeit ab wann die Eingabe frei ist 
+    
+    #länge der Maske 
+    
+    #Trials 100
+    trials = 100
+    #Testtrials 5
+    
+    #Zeit Feedback
+    feedback = 0.8
+    
+    
+    
