4a7c8ac795d812ca1e399833302583b36b7b1c64
ItLordLeon <leonphilipp.berberich@web.de>
2018-08-28 17:15:37 +0200

    set an zeug
    asdf

--- Variablen1.py
+++ Variablen1.py
@@ -8,16 +8,18 @@ Created on Thu Jun 07 17:22:56 2018
 class Variables:
     
     #0...255 Helligkeitsunterschied zwischen Rauschen und Bild 
-    kontrastDesZeichens = 5
+    kontrastDesZeichens = 6
+    
     
     #Bild muss im Ordner des Projekts liegen
-    bildname = "Ababa.jpg"
+    bildname = "Ababa - Kopie.jpg"
+    #Ababa (256) und Ababa - Kopie (64) und Ababa 128 (128)
     
     ##Rauschmatrix
     #Mittelwert
-    mittelwert = 170
+    mittelwert = 130
     #Standartabweichung
-    standartabweichung = 20
+    standartabweichung = 25
     
     #Zeit ab welcher der versuch als ungültig markiert
     
@@ -26,24 +28,25 @@ class Variables:
     maxKontrast = 10
     #Zeit ab wann die Eingabe frei ist 
     
-    
+    #Pixel pix x pix 
+    pix = 64
     
     #Trials 100
 
     trials = 5
 
     #Zeit 
-    fixationskreuz = 0
+    fixationskreuz = 0.25
     
     #länge der Maske 
-    maske=0
+    maske=0.3
     
     #Zeit des Stimulus
-    stimulusZeit= 1
+    stimulusZeit= 2.0/60.0
     #Antwortperiode
     antwortperiode= 2
     #Zeit Feedback
-    feedback = 0.5
+    feedback = 1
     #Pause
     pause = 0.5
  
