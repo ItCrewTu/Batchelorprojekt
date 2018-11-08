545d632852273c262a86c976c707f948503fa594
felixschmitt <39525983+felixschmitt@users.noreply.github.com>
2018-11-08 16:16:37 +0100

    Revert "Update Gui.py"
    This reverts commit 57d81307d6df8a43657f2b4e3f8fde7d1d49464b.

--- Gui.py
+++ Gui.py
@@ -1,77 +1,75 @@
 # -*- coding: utf-8 -*-
-"""
-provides the functions to create a gui
-
-"""
 import os
+from psychopy import core, gui
 import sys
-from psychopy import gui
+"""
+Created on Thu Sep 06 16:32:11 2018
 
+@author: Leon
+"""
+"""
+###############################################################################
+############### Verzeichnis alter und neuer Variablennamen ####################
+###############################################################################
 
+guiInput = eingabe
+thisDir = _thisDir
+
+"""
 class StateCheckIn:
-    """
-    provides the functions to create a gui
+
+    #L if this class is started, it open up our Gui 
+    def checkInNameAndType (self):
+        #L ensure that the path starts from the same directery this script is in
+        self.thisDir = os.path.dirname(os.path.abspath(__file__)).decode(sys.getfilesystemencoding())
+        os.chdir(self.thisDir)
+        
+        
+        ### Eingabefenster für Daten der Vpn, automatisch beim Start geöffnet
+        self.guiInput = gui.Dlg(title="Signalentdeckung.py")
+        print(33)
+        self.guiInput.addField("Versuchsperson:")##0
+        self.guiInput.addField("Durchgang:") ##1
+        self.guiInput.addField("Experimenttyp:", choices = ["Yes/No Task", "2IFC", "4IFC", "Constant Stimuli"]) ##2
+        self.guiInput.show()
+        
+    def setVariables (self, experimentType):
+        
+        self.guiInputVar = gui.Dlg(title="Signalentdeckung.py")
+        self.guiInputVar.addText("Einstellungen:")
     
-    """
+        self.guiInputVar.addField("Trialanzahl",10) ##3
+        self.guiInputVar.addField("Anzahl der Trialblocks", 5)##4"
+        self.guiInputVar.addField("Anzahl der Testtrials",5) ##5
+        self.guiInputVar.addField("Dauer Fixationskreuz",0.2)  ##5 +1 nach unte     
+        self.guiInputVar.addField("Dauer Maske:",0.1) ##6
+        self.guiInputVar.addField("Dauer Stimulus",0.2) ##7
+        self.guiInputVar.addField("Dauer Antwortperiode",2) ##8
+        self.guiInputVar.addField("Dauer Feedback",0.2) ##9
+        self.guiInputVar.addField("Dauer Pause",0.1) ##10
+        
+    #    guiInput.addField("Zufällig:",False)##17
+    #    guiInput.show()
+    #    if guiInput.data[11] == True:
+        
+        self.guiInputVar.addField("Stimulusgröße in Pixeln", choices = ["64x64","128x128","256x256"]) ##11
+        self.guiInputVar.addText("Signaleinstellungen:")
+    
+        self.guiInputVar.addField("Stärke des Signals:",11) ##12
+        self.guiInputVar.addField("Mittelwert",128) ##13
+    
+        self.guiInputVar.addField("Standartabweichung",30) ##14
+        if(experimentType == "Constant Stimuli"):
+            self.guiInputVar.show()
+    #    guiInput.addField("Trialablauf",) #
+        self.guiInputVar.addField("Kontrast - X je Trialblock",False)##15
+        self.guiInputVar.addField("Schrittweite",2)##16 
+        self.guiInputVar.addField("Zufällig",False)##17 nicht immer (zufall wählbar)
+        if (experimentType != "Constant Stimuli"):
+            self.guiInputVar.show()
 
-    # if this class is started, it opens the gui
-    def check_in_name_and_type(self):
-        """
-        creates a gui for testperson data and experiment type
-        
-        """
-        # ensure that the path starts from the same directery this script is in
-        self.this_dir = os.path.dirname(
-            os.path.abspath(__file__)).decode(
-                sys.getfilesystemencoding())
-        os.chdir(self.this_dir)
 
-        # input window for the data of this testperson
-        self.gui_input = gui.Dlg(title="Signalentdeckung.py")
-        self.gui_input.addField("Versuchsperson:")  # 0
-        self.gui_input.addField("Durchgang:")  # 1
-        self.gui_input.addField(
-            "Experimenttyp:",
-            choices=[
-                "Yes/No Task",
-                "2IFC",
-                "4IFC",
-                "Constant Stimuli"])  # 2
-        self.gui_input.show()
 
-    def set_variables(self, experiment_type):
-        """
-        creates a gui to configure the experiment
-        
-        """
-
-        self.gui_input_var = gui.Dlg(title="Signalentdeckung.py")
-        
-        self.gui_input_var.addText("Einstellungen:")
-
-        self.gui_input_var.addField("Trialanzahl", 10)  # 3
-        self.gui_input_var.addField("Anzahl der Trialblocks", 5)  # 4
-        self.gui_input_var.addField("Anzahl der Testtrials", 5)  # 5
-        self.gui_input_var.addField("Dauer Fixationskreuz", 0.2)  # 6
-        self.gui_input_var.addField("Dauer Maske:", 0.1)  # 7
-        self.gui_input_var.addField("Dauer Stimulus", 0.2)  # 8
-        self.gui_input_var.addField("Dauer Antwortperiode", 2)  # 9
-        self.gui_input_var.addField("Dauer Feedback", 0.2)  # 10
-        self.gui_input_var.addField("Dauer Pause", 0.1)  # 11
-        self.gui_input_var.addField("Stimulusgröße in Pixeln",
-                                    choices=["64x64", "128x128", "256x256"])  # 12
-        
-        self.gui_input_var.addText("Signaleinstellungen:")
-        
-        self.gui_input_var.addField("Stärke des Signals:", 11)  # 13
-        self.gui_input_var.addField("Mittelwert", 128)  # 14
-        self.gui_input_var.addField("Standartabweichung", 30)  # 15
-        
-        # dont show the last 3 options for the constant stimuli task 
-        if experiment_type == "Constant Stimuli":
-            self.gui_input_var.show()
-        self.gui_input_var.addField("Kontrast - X je Trialblock", False)  # 16
-        self.gui_input_var.addField("Schrittweite", 2)  # 17
-        self.gui_input_var.addField("Zufällig", False)
-        if experiment_type != "Constant Stimuli":
-            self.gui_input_var.show()
+    # Abbruch falls Cancel gedrückt wurde
+#    if eingabe.OK == False:
+#        core.quit()
